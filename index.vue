93d8c4fc8be8bae93bc81f3f5834dbfd1fc0a17e
degfy <degfy@sina.com>
2016-12-30 15:11:41 +0800

    update

---
+++ index.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="ps-scoller">
+    <slot></slot>
+  </section>
+</template>
+<style lang="scss">
+@import '~perfect-scrollbar/src/css/main.scss';
+</style>
+<script>
+import PsScroll from 'perfect-scrollbar'
+
+function vuePScroller(settings) {
+  return {
+    methods: {},
+    mounted() {
+      PsScroll.initialize(this.$el, settings)
+    },
+    beforeDestroy() {
+      PsScroll.destroy(this.$el)
+    }
+  }
+}
+export default vuePScroller
+</script>
