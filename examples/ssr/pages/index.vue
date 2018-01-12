185d61631f8f05ec91ef938ef58ab0f34ca5fdc5
degfy <degfy@sina.com>
2018-01-12 10:11:51 +0800

    use umd export with webpack

---
+++ examples/ssr/pages/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>Hello world!</h1>
+    <Ps class="wrap" :settings="settings" @ps-scroll-y="evtTest">
+      <h2 v-for="i in 100"> this is a test;test test test test test test test test </h2>
+      <div>author:degfy</div>
+    </Ps>
+  </div>
+</template>
+<script>
+import Ps from 'vue-perfect-scrollbar'
+
+export default {
+  components: {
+    Ps,
+  },
+  data() {
+    return {
+      settings: {
+        wheelSpeed: 2,
+        wheelPropagation: true,
+        minScrollbarLength: 20,
+      }
+    }
+  },
+
+  methods: {
+    evtTest(evt) {
+      console.log(evt)
+    }
+  },
+}
+</script>
+<style>
+.wrap {
+  width: 400px;
+  height: 300px;
+  overflow: hidden;
+  border: 1px solid red;
+}
+
+.wrap h2 {
+  word-break: none;
+  white-space: nowrap;
+}
+</style>
--- index.js
+++ index.js
@@ -2,115 +2,124 @@ import 'perfect-scrollbar/css/perfect-scrollbar.css'
 import './index.css'
 import PerfectScrollbar from 'perfect-scrollbar'
 
-export default {
-  name: 'vue-perfect-scrollbar',
-  props: {
-    settings: {
-      type: Object,
-      default () {
-        return {}
+export default 
+
+// require('perfect-scrollbar/css/perfect-scrollbar.css')
+// require('./index.css')
+
+// const PerfectScrollbar = require('perfect-scrollbar')
+
+// module.exports =
+
+  {
+    name: 'vue-perfect-scrollbar',
+    props: {
+      settings: {
+        type: Object,
+        default () {
+          return {}
+        },
+      },
+
+      swicher: {
+        type: Boolean,
+        default: true,
+      },
+
+      tag: {
+        type: String,
+        default: 'div',
+      }
+    },
+
+    methods: {
+
+      __init() {
+        if (!this._ps_bar)
+          this._ps_bar = new PerfectScrollbar(this.$el, this.settings)
+      },
+
+      __uninit() {
+        if (this._ps_bar) {
+          this._ps_bar.destroy()
+          this._ps_bar = null
+        }
+      },
+
+      __update() {
+        if (this._ps_bar) {
+          this._ps_bar.update()
+        }
+      },
+
+      //deal event 
+      /*
+        ps-scroll-y
+        ps-scroll-x
+        ps-scroll-up
+        ps-scroll-down
+        ps-scroll-left
+        ps-scroll-right
+        ps-y-reach-start
+        ps-y-reach-end
+        ps-x-reach-start
+        ps-x-reach-end
+       */
+      __evt_handle(evt) {
+        this.$emit(evt.type, evt)
+      },
+
+      update() {
+        this.__update()
       },
     },
-    
-    swicher: {
-      type: Boolean,
-      default: true,
-    },
 
-    tag: {
-      type: String,
-      default: 'div',
-    }
-  },
-
-  methods: {
-
-    __init() {
-      if (!this._ps_bar)
-        this._ps_bar = new PerfectScrollbar(this.$el, this.settings)
-    },
-
-    __uninit() {
-      if (this._ps_bar) {
-        this._ps_bar.destroy()
-        this._ps_bar = null
+    mounted() {
+      if (!this.$isServer) {
+        this.__init()
       }
     },
 
-    __update() {
-      if (this._ps_bar) {
-        this._ps_bar.update()
-      }
+    updated() {
+      this.$nextTick(this.update)
     },
 
-    //deal event 
-    /*
-      ps-scroll-y
-      ps-scroll-x
-      ps-scroll-up
-      ps-scroll-down
-      ps-scroll-left
-      ps-scroll-right
-      ps-y-reach-start
-      ps-y-reach-end
-      ps-x-reach-start
-      ps-x-reach-end
-     */
-    __evt_handle(evt) {
-      this.$emit(evt.type, evt)
-    },
-
-    update() {
-      this.__update()
-    },
-  },
-
-  mounted() {
-    if (!this.$isServer) {
+    activated() {
       this.__init()
-    }
-  },
+    },
 
-  updated() {
-    this.$nextTick(this.update)
-  },
+    deactivated() {
+      this.__uninit()
+    },
 
-  activated() {
-    this.__init()
-  },
+    beforeDestroy() {
+      this.__uninit()
+    },
 
-  deactivated() {
-    this.__uninit()
-  },
+    render(createElement, context) {
 
-  beforeDestroy() {
-    this.__uninit()
-  },
+      const data = {
+        class: {
+          'v-p-s': true,
+        },
+        on: {
+          'ps-scroll-y': this.__evt_handle,
+          'ps-scroll-x': this.__evt_handle,
+          'ps-scroll-up': this.__evt_handle,
+          'ps-scroll-down': this.__evt_handle,
+          'ps-scroll-left': this.__evt_handle,
+          'ps-scroll-right': this.__evt_handle,
+          'ps-y-reach-start': this.__evt_handle,
+          'ps-y-reach-end': this.__evt_handle,
+          'ps-x-reach-start': this.__evt_handle,
+          'ps-x-reach-end': this.__evt_handle,
+        },
+      }
 
-  render(createElement, context) {
-
-    const data = {
-      class: {
-        'v-p-s': true,
-      },
-      on: {
-        'ps-scroll-y': this.__evt_handle,
-        'ps-scroll-x': this.__evt_handle,
-        'ps-scroll-up': this.__evt_handle,
-        'ps-scroll-down': this.__evt_handle,
-        'ps-scroll-left': this.__evt_handle,
-        'ps-scroll-right': this.__evt_handle,
-        'ps-y-reach-start': this.__evt_handle,
-        'ps-y-reach-end': this.__evt_handle,
-        'ps-x-reach-start': this.__evt_handle,
-        'ps-x-reach-end': this.__evt_handle,
-      },
-    }
-
-    return createElement(
-      this.tag,
-      data,
-      this.$slots.default
-    )
-  },
-}
+      return createElement(
+        this.tag,
+        data,
+        this.$slots.default
+      )
+    },
+  }
