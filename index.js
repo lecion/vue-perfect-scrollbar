3a1d61db258c9d566aa40a2de1e6c863b118aa50
degfy <degfy@sina.com>
2018-01-12 10:39:17 +0800

    add method:scrollTop

--- index.js
+++ index.js
@@ -2,124 +2,108 @@ import 'perfect-scrollbar/css/perfect-scrollbar.css'
 import './index.css'
 import PerfectScrollbar from 'perfect-scrollbar'
 
-export default 
-
-// require('perfect-scrollbar/css/perfect-scrollbar.css')
-// require('./index.css')
-
-// const PerfectScrollbar = require('perfect-scrollbar')
-
-// module.exports =
-
-  {
-    name: 'vue-perfect-scrollbar',
-    props: {
-      settings: {
-        type: Object,
-        default () {
-          return {}
-        },
+export default {
+  name: 'vue-perfect-scrollbar',
+  props: {
+    settings: {
+      type: Object,
+      default () {
+        return {}
       },
+    },
 
-      swicher: {
-        type: Boolean,
-        default: true,
-      },
+    swicher: {
+      type: Boolean,
+      default: true,
+    },
 
-      tag: {
-        type: String,
-        default: 'div',
+    tag: {
+      type: String,
+      default: 'div',
+    },
+  },
+
+  methods: {
+
+    __init() {
+      if (!this._ps_bar)
+        this._ps_bar = new PerfectScrollbar(this.$el, this.settings)
+    },
+
+    __uninit() {
+      if (this._ps_bar) {
+        this._ps_bar.destroy()
+        this._ps_bar = null
       }
     },
 
-    methods: {
-
-      __init() {
-        if (!this._ps_bar)
-          this._ps_bar = new PerfectScrollbar(this.$el, this.settings)
-      },
-
-      __uninit() {
-        if (this._ps_bar) {
-          this._ps_bar.destroy()
-          this._ps_bar = null
-        }
-      },
-
-      __update() {
-        if (this._ps_bar) {
-          this._ps_bar.update()
-        }
-      },
-
-      //deal event 
-      /*
-        ps-scroll-y
-        ps-scroll-x
-        ps-scroll-up
-        ps-scroll-down
-        ps-scroll-left
-        ps-scroll-right
-        ps-y-reach-start
-        ps-y-reach-end
-        ps-x-reach-start
-        ps-x-reach-end
-       */
-      __evt_handle(evt) {
-        this.$emit(evt.type, evt)
-      },
-
-      update() {
-        this.__update()
-      },
-    },
-
-    mounted() {
-      if (!this.$isServer) {
-        this.__init()
+    __update() {
+      if (this._ps_bar) {
+        this._ps_bar.update()
       }
     },
 
-    updated() {
-      this.$nextTick(this.update)
+    __evt_handle(evt) {
+      this.$emit(evt.type, evt)
     },
 
-    activated() {
+    update() {
+      this.__update()
+    },
+
+    scrollTop(top) {
+      if (this._ps_bar) {
+        this.$el.scrollTop = top
+      }
+    },
+  },
+
+  mounted() {
+    if (!this.$isServer) {
       this.__init()
-    },
+    }
+  },
 
-    deactivated() {
-      this.__uninit()
-    },
+  updated() {
+    this.$nextTick(this.update)
+  },
 
-    beforeDestroy() {
-      this.__uninit()
-    },
+  activated() {
+    this.__init()
+  },
 
-    render(createElement, context) {
+  deactivated() {
+    this.__uninit()
+  },
 
-      const data = {
-        class: {
-          'v-p-s': true,
-        },
-        on: {
-          'ps-scroll-y': this.__evt_handle,
-          'ps-scroll-x': this.__evt_handle,
-          'ps-scroll-up': this.__evt_handle,
-          'ps-scroll-down': this.__evt_handle,
-          'ps-scroll-left': this.__evt_handle,
-          'ps-scroll-right': this.__evt_handle,
-          'ps-y-reach-start': this.__evt_handle,
-          'ps-y-reach-end': this.__evt_handle,
-          'ps-x-reach-start': this.__evt_handle,
-          'ps-x-reach-end': this.__evt_handle,
-        },
-      }
+  beforeDestroy() {
+    this.__uninit()
+  },
 
-      return createElement(
-        this.tag,
-        data,
-        this.$slots.default
-      )
-    },
-  }
+  render(createElement, context) {
+
+    const data = {
+      class: {
+        'v-p-s': true,
+      },
+      on: {
+        'ps-scroll-y': this.__evt_handle,
+        'ps-scroll-x': this.__evt_handle,
+        'ps-scroll-up': this.__evt_handle,
+        'ps-scroll-down': this.__evt_handle,
+        'ps-scroll-left': this.__evt_handle,
+        'ps-scroll-right': this.__evt_handle,
+        'ps-y-reach-start': this.__evt_handle,
+        'ps-y-reach-end': this.__evt_handle,
+        'ps-x-reach-start': this.__evt_handle,
+        'ps-x-reach-end': this.__evt_handle,
+      },
+    }
+
+    return createElement(
+      this.tag,
+      data,
+      this.$slots.default
+    )
+  },
+}
